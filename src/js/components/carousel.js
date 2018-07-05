ec67dceb5160021dd981dbbd74e777f4c85ca2d3
4lg4 <akgleal@gmail.com>
2018-07-06 09:47:41 +1000

    new carousel using style animations

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -11,6 +11,7 @@ class Carousel extends Component {
     this.state = {};
 
     this.movieContainer = React.createRef();
+    this.movieContainerRail = React.createRef();
     this.handleOnSelect = this.handleOnSelect.bind(this);
     this.handleScrollTo = this.handleScrollTo.bind(this);
   }
@@ -21,15 +22,45 @@ class Carousel extends Component {
 
   componentDidUpdate() {
     this.movieContainer.current.scrollLeft = 0;
+
+    const movieContainer = this.movieContainer.current;
+    const movieContainerRail = this.movieContainerRail.current;
+    console.log('componentDidUpdate ', movieContainer.offsetWidth, movieContainerRail.style.left, movieContainerRail.offsetWidth);
+  }
+
+  componentDidMount() {
+    const movieContainer = this.movieContainer.current;
+    const movieContainerRail = this.movieContainerRail.current;
+    console.log('componentDidMount ', movieContainer.offsetWidth, movieContainerRail.style.left, movieContainerRail.offsetWidth);
   }
 
   handleScrollTo(direction) {
     const movieContainer = this.movieContainer.current;
+    const movieContainerRail = this.movieContainerRail.current;
 
+    // console.log(this.movieContainerRail);
+    // console.log(movieContainerRail);
+    console.log(movieContainer.offsetWidth, movieContainerRail.style.left, movieContainerRail.offsetWidth);
+    // console.log(movieContainerRail.style.left += movieContainer.offsetWidth);
+    const left = (movieContainerRail.style.left) ? parseInt(movieContainerRail.style.left.replace('px', '')) : 0;
     if (direction === 'R') {
-      movieContainer.scrollLeft += movieContainer.offsetWidth;
+      // console.log(left + movieContainer.offsetWidth);
+      // console.log(movieContainerRail.offsetWidth, left - movieContainer.offsetWidth, (Math.abs(left - movieContainer.offsetWidth) >= (movieContainerRail.offsetWidth - movieContainer.offsetWidth)));
+      if (Math.abs(left - movieContainer.offsetWidth) >= (movieContainerRail.offsetWidth - movieContainer.offsetWidth)) {
+        movieContainerRail.style.left = `auto`;
+        movieContainerRail.style.right = `0px`;
+      } else {
+        movieContainerRail.style.right = `auto`;
+        movieContainerRail.style.right = `${left - movieContainer.offsetWidth}px`;
+      }
+      console.log('scroll R', scroll);
+      movieContainerRail.style.left = `${scroll}px`;
+      // movieContainer.scrollLeft += movieContainer.offsetWidth;
     } else {
-      movieContainer.scrollLeft -= movieContainer.offsetWidth;
+      console.log(left + movieContainer.offsetWidth, (Math.abs(left + movieContainer.offsetWidth) >= movieContainerRail.offsetWidth));
+      const scroll = (Math.abs(left + movieContainer.offsetWidth) >= movieContainerRail.offsetWidth) ? movieContainerRail.offsetWidth : (left + movieContainer.offsetWidth);
+      movieContainerRail.style.left = `${scroll}px`;
+      // movieContainer.scrollLeft -= movieContainer.offsetWidth;
     }
   }
 
@@ -43,10 +74,12 @@ class Carousel extends Component {
     return (
       <div className="carousel">
         <div className="carousel__nav-left" onClick={()=> this.handleScrollTo('L')}>L</div>
-        <div className="carousel__content" ref={this.movieContainer}>
-          {items.map((item, i) =>
-            <MoviePoster key={i} image={item.Poster} onClick={()=> this.handleOnSelect(item)} />
-          )}
+        <div className="carousel__container" ref={this.movieContainer}>
+          <div className="carousel__container__rail" ref={this.movieContainerRail}>
+            {items.map((item, i) =>
+              <MoviePoster key={i} image={item.Poster} onClick={()=> this.handleOnSelect(item)} />
+            )}
+          </div>
         </div>
         <div className="carousel__nav-right" onClick={()=> this.handleScrollTo('R')}>R</div>
       </div>
